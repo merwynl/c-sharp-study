325752190e262ba73fa5e9cb98fa6c32fe99845d
まーういん <merwyn.lim@gmail.com>
2025-07-23 13:57:49 +1000

    update entry on multithreading

--- c-sharp-study/multithreading.cs
+++ c-sharp-study/multithreading.cs
@@ -10,6 +10,38 @@ public class multithreading
      */
     static void Main(string[] args)
     {
+        Thread mainThread = Thread.CurrentThread;
+        mainThread.Name = "Main Thread";
+        Console.WriteLine("Main thread Name: {0}", mainThread.Name);
         
+        Thread thread1 = new Thread(() =>CountDown("Timer #1"));
+        Thread thread2 = new Thread(()=>CountUp("Timer #2"));
+
+        thread1.Start();
+        thread2.Start();
+        
+        // Console.WriteLine("Main thread: " + mainThread.Name + " is finished");
+    }
+
+    public static void CountDown(String name)
+    {
+        for (int i = 10; i >= 0; i--)
+        {
+            Console.WriteLine("Timer #1: " + i + " seconds");
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine("Timer #1 has finished.");
+        
+        Console.ReadKey();
+    }
+    
+    public static void CountUp(String name)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Timer #2: " + i + " seconds");
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine("Timer #2 has finished.");
     }
 }
